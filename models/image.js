504e28dbd7a8c0f289a877ee3db7f6e917491e04
humphreyokoth <humpasp@gmail.com>
2023-10-18 07:40:09 +0300

    Fix: image model

--- models/image.js
+++ models/image.js
@@ -1,13 +1,18 @@
-module.exports = (sequelize,Sequelize)=>{
-
-    const Image = sequelize.define("images",{
-       type:{
-        id:DataTypes.INTEGER,
-
-       } ,
-       name:{
-        type:DataTypes.STRING,
-       }
-    })
-    return Image;
-}
+module.exports = (sequelize, Sequelize) => {
+  const Image = sequelize.define("images", {
+    type: {
+      id: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    imageName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    imagePath: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  });
+  return Image;
+};
